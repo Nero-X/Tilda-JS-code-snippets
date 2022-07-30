029e9850bb78e5dadba392d85870aa67c72b2d3e
Nero-X <SashaJCV@yandex.ru>
2022-07-30 23:02:58 +0300

    * Добавлены папки для разных сайтов
    * Добавлен danilmoscovchenko.js

---
+++ danilmoscovchenko/danilmoscovchenko.js
@@ -0,0 +1,56 @@
+$(function() {
+    let animStyle = document.createElement("style");
+    animStyle.innerHTML = `
+    .flash-animation {
+        position: relative;
+        overflow: hidden;
+        -webkit-transform: translate3d(0,0,0);
+        transform: translate3d(0,0,0);
+    }
+
+    .flash-animation-effect {
+        background: -webkit-gradient(linear, left top, right top, from(rgba(255, 255, 255, .1)), to(rgba(255, 255, 255, .4)));
+        background: -webkit-linear-gradient(left, rgba(255, 255, 255, .1), rgba(255, 255, 255, .4));
+        background: -o-linear-gradient(left, rgba(255, 255, 255, .1), rgba(255, 255, 255, .4));
+        background: linear-gradient(90deg, rgba(255, 255, 255, .1), rgba(255, 255, 255, .4));
+        width: 45px;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 30px;
+        -webkit-transform: skewX(-45deg);
+        -ms-transform: skewX(-45deg);
+        transform: skewX(-45deg);
+    }
+
+    .flash-animation-wrap {
+        -webkit-animation-name: flash-md;
+        animation-name: flash-md;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        -webkit-transform: translateX(-85px);
+        -ms-transform: translateX(-85px);
+        transform: translateX(-85px);
+        -webkit-animation-name: flash;
+        animation-name: flash;
+        -webkit-animation-duration: 3s;
+        animation-duration: 3s;
+        -webkit-animation-timing-function: linear;
+        animation-timing-function: linear;
+        -webkit-animation-iteration-count: infinite;
+        animation-iteration-count: infinite;
+    }
+    `;
+    document.head.appendChild(animStyle);
+
+    $("div[data-elem-type=button].t396__elem").each(function() {
+        this.classList.add("flash-animation");
+        this.insertAdjacentHTML("beforeend", '<div class="flash-animation-wrap"><div class="flash-animation-effect"></div></div>');
+        $("div.t396__elem[data-elem-type=button] .tn-atom, button.t-submit").css({
+            "background": "linear-gradient(90deg, rgb(254,220,123) 0%, rgb(176,131,69) 100%)"
+        });
+    });
+})
