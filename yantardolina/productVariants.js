9e2d2c2e95cf6185d05e02afbeb520be1d3c964e
Nero-X <SashaJCV@yandex.ru>
2024-09-29 04:00:05 +0300

    * Добавлено ожидание кнопок

--- yantardolina/productVariants.js
+++ yantardolina/productVariants.js
@@ -12,33 +12,54 @@ function generateGUID(nDigits, base = 10) {
     return guid;
 }
 
+function waitForElms(selector, parent = document) {
+    return new Promise(resolve => {
+        if (parent.querySelector(selector)) {
+            return resolve(parent.querySelectorAll(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (parent.querySelector(selector)) {
+                resolve(parent.querySelectorAll(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(parent, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
 $(function() {
     let style = document.createElement("style");
     style.innerHTML = css;
     document.head.appendChild(style);
 
-    let verBtns = $(".t396 .t396__elem[class*=ver]");
-    let buyBtns = $(".t1093 .v-order");
+    waitForElms(".t396 .t396__elem[class*=ver]").then(function(verBtns) {
+        $(verBtns).on("click", function() {
+            verBtns.removeClass("ver-selected");
+            $(this).addClass("ver-selected");
+        });
+    })
 
-    verBtns.on("click", function() {
-        verBtns.removeClass("ver-selected");
-        $(this).addClass("ver-selected");
-    });
-
-    buyBtns.on("click", function() {
-        let popup = $(this).closest(".t1093");
-        let product = {
-            name: popup.find(".t396__elem.name").text(),
-            url: "https://yantardolina.ru/oil",
-            img: popup.find(".t396__elem.v-img1 img").attr('src'),
-            options: [{
-                option: "Объём",
-                price: 0,
-                variant: verBtns.filter(".ver-selected").text()
-            }],
-            uid: generateGUID(10),
-            price: ""
-        }
-        tcart__addProduct(product)
-    });
+    waitForElms(".t1093 .v-order").then(function(buyBtns) {
+        buyBtns.on("click", function() {
+            let popup = $(this).closest(".t1093");
+            let product = {
+                name: popup.find(".t396__elem.name").text(),
+                url: "https://yantardolina.ru/oil",
+                img: popup.find(".t396__elem.v-img1 img").attr('src'),
+                options: [{
+                    option: "Объём",
+                    price: 0,
+                    variant: verBtns.filter(".ver-selected").text()
+                }],
+                //uid: generateGUID(10),
+                price: ""
+            }
+            tcart__addProduct(product)
+        });
+    })
 })
