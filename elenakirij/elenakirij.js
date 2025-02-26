51d011eacc5e21fcdb8f8aa5853219ba3b3de206
Nero-X <47949085+Nero-X@users.noreply.github.com>
2025-02-26 13:58:26 +0300

    Update elenakirij.js

--- elenakirij/elenakirij.js
+++ elenakirij/elenakirij.js
@@ -28,68 +28,103 @@ function waitUntil(conditionFunc, timeout) {
 }
 
 let css = `
-div[class*="situation-button-arrow-"] {
-    transition: transform 1s;
+div[class*="situation-button-arrow-"] .tn-atom__img {
+    transition: transform .3s;
 }
-.situation-button-text {
+div .situation-button-text {
     pointer-events: none;
 }
+span.gradient {
+    color: transparent;
+    background-image: linear-gradient(0.912turn, rgba(238, 69, 231, 1) 0%, rgba(128, 52, 248, 1) 100%);
+    -webkit-background-clip: text;
+}
 `;
+
+function selectSituationButton(index, textBlocks, buttonBlocks, buttonArrows){
+    $(buttonBlocks[index]).find(".tn-atom").css("background", "none");
+    $(buttonArrows[index]).find(".tn-atom__img").css("transform", "rotate(180deg)");
+    $(textBlocks[index]).find(".tn-atom").css("opacity", "1");
+}
+
+function deselectAllSituationButtons(textBlocks, buttonBlocks, buttonArrows){
+    $(buttonBlocks).find(".tn-atom").css("background", "");
+    $(buttonArrows).find(".tn-atom__img").css("transform", "");
+    $(textBlocks).find(".tn-atom").css("opacity", "0");
+}
 
 $(function() {
     let style = document.createElement("style");
     style.innerHTML = css;
     document.head.appendChild(style);
 
-    let textBlocks, buttonBlocks, buttonArrows = [];
+    // text gradient
+    let spans = [
+        ["#rec869953924", "говорят: “здоров”"],
+        ["#rec869953924", "плохо себя чувствуешь"],
+        ["#rec870668587", "без эмоций"],
+        ["#rec870670813", "Методики"],
+        ["#rec871079669", "одна коррекция"],
+        ["#rec859540749", "Елена Кирий"],
+        ["#rec871037305", "принимаю в&nbsp;Краснодаре"],
+        ["#rec870670647", "Пространство"],
+        ["#rec870670647", "безопасности"],
+        ["#rec870424746", "истории"],
+        ["#rec870421978", "ловцы солнца и&nbsp;пирсинг"],
+        ["#rec870327040", "ответ на"],
+        ["#rec870327040", "вопрос"],
+        ["#rec870323912", "Контакты"]
+    ];
+    spans.forEach(element => {
+        let html = $(element[0]).html();
+        let index = html.indexOf(element[1]);
+        if (index > -1) {
+            html = `${html.slice(0, index)}<span class="gradient">${element[1]}</span>${html.slice(index + element[1].length)}`;
+            $(element[0]).html(html);
+        }
+    });
+
+    // collect elements
+    let textBlocksDict = {};
+    let buttonBlocksDict= {};
+    let buttonArrowsDict = {};
     waitForElms('div[class^="t396__elem tn-elem situation-"]').then((elms) => {
         $(elms).each((index, element) => {
-            switch (this.className.match(/situation-/)) {
-                case value:
-                    
-                    break;
-            
-                default:
-                    break;
+            let situationClassNameArr = element.className.match(/situation-\S+/)[0].split("-");
+            if (situationClassNameArr[1] == "text") {
+                textBlocksDict[situationClassNameArr[2]] = element;
             }
-        })
-        let textRegex = /situation-text-\d+/;
-        let elmsArray = Array.from(elms);
-        textBlocks = elmsArray.sort((a, b) => a.className.match(textRegex)[0].split('-').slice(-1) - b.className.match(textRegex)[0].split('-').slice(-1));
-    });
+            else if (situationClassNameArr[2] >= "0" && situationClassNameArr[2] <= 9) {
+                buttonBlocksDict[situationClassNameArr[2]] = element;
+            }
+            else if (situationClassNameArr[2] == "arrow") {
+                buttonArrowsDict[situationClassNameArr[3]] = element;
+            }
+        });
 
+        // sort
+        let textBlocks = [];
+        let buttonBlocks= [];
+        let buttonArrows = [];
+        var keys = Object.keys(buttonBlocksDict);
+        keys.sort();
+        for (var i=0; i<keys.length; i++) { 
+            var key = keys[i];
+            textBlocks.push(textBlocksDict[key]);
+            buttonBlocks.push(buttonBlocksDict[key]);
+            buttonArrows.push(buttonArrowsDict[key]);
+        }
 
+        //assign styles
+        $(buttonBlocks).css("cursor", "pointer");
+        selectSituationButton(0, textBlocks, buttonBlocks, buttonArrows);
 
-
-
-    waitForElms('div[class^="t396__elem tn-elem situation-text-"]').then((elms) => {
-        let textRegex = /situation-text-\d+/;
-        let elmsArray = Array.from(elms);
-        textBlocks = elmsArray.sort((a, b) => a.className.match(textRegex)[0].split('-').slice(-1) - b.className.match(textRegex)[0].split('-').slice(-1));
-    });
-    waitForElms('div[class^="t396__elem tn-elem situation-button-"]').then((elms) => {
-        let buttonRegex = /situation-button-\d+/;
-        let elmsArray = Array.from(elms);
-        console.log(elmsArray);
-        buttonBlocks = elmsArray.sort((a, b) => a.className.match(buttonRegex)[0].split('-').slice(-1) - b.className.match(buttonRegex)[0].split('-').slice(-1));
-    });
-    waitForElms('div[class^="t396__elem tn-elem situation-button-arrow-"]').then((elms) => {
-        let arrowRegex = /situation-button-arrow-\d+/;
-        let elmsArray = Array.from(elms);
-        buttonArrows = elmsArray.sort((a, b) => a.className.match(arrowRegex)[0].split('-').slice(-1) - b.className.match(arrowRegex)[0].split('-').slice(-1));
-    });
-
-    waitUntil(() => textBlocks !== undefined && buttonBlocks !== undefined && buttonArrows !== undefined, 1000).then(() => {
+        // assign events
         $(buttonBlocks).each((index, element) => {
             $(element).on("click", function() {
-                $(buttonBlocks).find(".tn-atom").css("background", "");
-                $(buttonArrows).find(".tn-atom__img").css("transform", "");
-                $(textBlocks).find(".tn-atom").css("opacity", "0");
-    
-                $(element).find(".tn-atom").css("background", "none");
-                $(buttonArrows[index]).find(".tn-atom__img").css("transform", "rotate(180deg)");
-                $(textBlocks[index]).find(".tn-atom").css("opacity", "1");
+                deselectAllSituationButtons(textBlocks, buttonBlocks, buttonArrows);
+                selectSituationButton(index, textBlocks, buttonBlocks, buttonArrows);
             })
         })
-    })
+    });
 })
